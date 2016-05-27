f1dfd7a7b4e1c7ecdcf03bf76665841ac4316678
JeanFulbert <christophe.moinard@meteodyn.com>
2016-05-27 10:42:52 +0200

    Solution
    Creating Solution and types

---
+++ StringTensionCalculator/StringTensionCalculator/Notes.fs
@@ -0,0 +1,12 @@
+﻿namespace StringTensionsCalculator
+
+module Notes =
+    let add (semitones:int) (note:Note) =
+        match semitones with
+        | s when s > 0 ->
+            match semitones with
+            | s when s % 12 = 0 -> { note with height = note.height + (s / 12) }
+            | s when note.name <> NoteName.GSharp && s < (12 - int note.name) -> { note with name = enum (s + int note.name) }
+            | _ -> note
+        | _ -> note
+
