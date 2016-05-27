db26380d1be105afd56568c0bc2c261485bd679b
JeanFulbert <christophe.moinard@meteodyn.com>
2016-05-27 11:09:25 +0200

    Move Solution
    Move all solution one level up

---
+++ StringTensionCalculator.Tests/NoteEqualitySpecs.fs
@@ -0,0 +1,13 @@
+﻿namespace StringTensionsCalculator.Tests
+
+open System
+open NUnit.Framework
+open Swensen.Unquote
+open StringTensionsCalculator
+open FsCheck
+open FsCheck.NUnit
+
+module NoteSpecs =
+    [<NoteProperty>]
+    let ``Note equals itself`` (note:Note) =
+        note =! note
---
+++ StringTensionCalculator/Types.fs
@@ -0,0 +1,40 @@
+﻿namespace StringTensionsCalculator
+
+open System
+
+type NoteName =
+|A = 0
+|ASharp = 1
+|B = 2
+|C = 3
+|CSharp = 4
+|D = 5
+|DSharp = 6
+|E = 7
+|F = 8
+|FSharp = 9
+|G = 10
+|GSharp = 11
+
+type NoteHeight = int
+
+[<CustomComparison; StructuralEquality>]
+type Note =
+    { name: NoteName
+      height: NoteHeight
+    }
+    interface IComparable with
+        member this.CompareTo other =
+            match other with
+            | :? Note as n -> compare (this.height, this.name) (n.height,n.name)
+            | _ -> invalidArg "other" "cannot compare value of different types"
+    override this.ToString() =
+        let n = this.name |> sprintf "%A"
+        let formattedName =
+            match n with
+            | x when x.EndsWith("Sharp") -> n.[0].ToString() + "#"
+            | _ -> n
+        formattedName + (this.height |> sprintf "%d")
+        
+
+[<Measure>] type hz
