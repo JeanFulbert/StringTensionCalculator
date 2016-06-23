5a0a695282c74893eb68f814312d0fb756df0ec3
Christophe Moinard <christophe.moinard@meteodyn.com>
2016-06-23 13:49:16 +0200

    Fix add negative semitones to notes without changing height

---
+++ StringTensionCalculator.Tests/NoteAddNegativeSpecs.fs
@@ -0,0 +1,54 @@
+﻿namespace StringTensionsCalculator.Tests
+
+open System
+open NUnit.Framework
+open Swensen.Unquote
+open StringTensionsCalculator
+open FsCheck
+open FsCheck.NUnit
+
+module NoteAddNegativeSpecs =
+    [<NoteProperty>]
+    let ``Adding -n semitones, where n is a multiple of 12, decrease the height by modulo`` (note:Note) =
+        (note.height > 0) ==> lazy
+
+        let octaveHeight = Gen.elements [1..note.height] |> Arb.fromGen
+        Prop.forAll octaveHeight (fun h ->
+            let semitonesToAdd = h * 12
+            let noteAfterAdded = note |> Notes.add -semitonesToAdd
+
+            noteAfterAdded.name =! note.name
+            noteAfterAdded.height =! note.height + h)
+
+    [<NoteProperty>]
+    let ``Adding -n semitones, where n is less than the name difference, decrease the name but not the height`` (note:Note) =
+        (note.name > NoteName.A) ==> lazy
+        
+        let nameAdd = Gen.elements [1..(int note.name)] |> Arb.fromGen
+        Prop.forAll nameAdd (fun n ->
+            let noteAfterNegativeAdded = note |> Notes.add -n
+
+            noteAfterNegativeAdded.height =! note.height
+            let nameAfterNegativeAdded = enum ((int note.name) - n)
+            noteAfterNegativeAdded.name =! nameAfterNegativeAdded)
+
+    [<NoteProperty>]
+    let ``Adding -n semitones, where n is higher than the difference between A and the note name, increase the name and the height`` (note:Note) =
+        (note.height > 0) ==> lazy
+
+        let diffToPreviousA = int note.name
+        let maxSemitonesAdd = note.height * 12 + int note.name
+
+        let semitonesAdd = Gen.elements [diffToPreviousA..maxSemitonesAdd] |> Arb.fromGen
+
+        Prop.forAll semitonesAdd (fun n ->
+            let actual = note |> Notes.add -n
+
+            let expectedHeightRemoved = ((n - diffToPreviousA - 1) / 12)
+            actual.height =! note.height - expectedHeightRemoved
+
+            let expectedNoteName =
+                match n % 12 with
+                | s when s <= diffToPreviousA -> enum (diffToPreviousA - s)
+                | s -> enum (11 - (s - diffToPreviousA))
+            actual.name =! expectedNoteName)
--- StringTensionCalculator/Notes.fs
+++ StringTensionCalculator/Notes.fs
@@ -1,7 +1,10 @@
 ﻿namespace StringTensionsCalculator
 
+open System
+
 module Notes =
     let add (semitones:int) (note:Note) =
+        let semitonesToPreviousA = -1 * int note.name
         let semitonesToNextA = 12 - int note.name
         match semitones with
         | 0 -> note
@@ -11,6 +14,15 @@ module Notes =
             | _ ->
                 let semitonesFromNextA = semitones - semitonesToNextA
                 let addHeight = (semitonesFromNextA / 12) + 1
-                let addNameFromNextA = semitonesFromNextA % 12
-                { name = enum addNameFromNextA; height = note.height + addHeight }
-        | _ -> note
+                let addNameFromA = semitonesFromNextA % 12
+                { name = enum addNameFromA; height = note.height + addHeight }
+        | _ ->
+            match semitones with
+            | s when s >= semitonesToPreviousA -> { note with name = enum (int note.name + s) }
+            | _ -> note
+            (*
+                let semitonesFromPreviousA = semitones - semitonesToPreviousA
+                let removeHeight = Math.Abs((semitonesFromPreviousA / 12) + -1)
+                let addNameFromA = Math.Abs(semitonesFromPreviousA % 12)
+                { name = enum addNameFromA; height = note.height - addHeight }
+            *)
