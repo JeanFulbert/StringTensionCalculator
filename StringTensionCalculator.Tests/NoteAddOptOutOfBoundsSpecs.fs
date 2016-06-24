8eb42148a2b76be5536eb665874edbf7330e4b0f
Christophe Moinard <christophe.moinard@meteodyn.com>
2016-06-24 14:28:14 +0200

    Add Notes.addOpt

---
+++ StringTensionCalculator.Tests/NoteAddOptOutOfBoundsSpecs.fs
@@ -0,0 +1,19 @@
+﻿namespace StringTensionsCalculator.Tests
+
+open System
+open NUnit.Framework
+open Swensen.Unquote
+open StringTensionsCalculator
+open FsCheck
+open FsCheck.NUnit
+
+module NoteAddOptOutOfBoundsSpecs =
+    [<NoteProperty>]
+    let ``Adding semitones higher than G#8 returns None`` (note:Note) =
+        let actual = note |> Notes.addOpt 108
+        actual =! None
+
+    [<NoteProperty>]
+    let ``Adding semitones lower than A0 returns None`` (note:Note) =
+        let actual = note |> Notes.addOpt -108
+        actual =! None
--- StringTensionCalculator/Notes.fs
+++ StringTensionCalculator/Notes.fs
@@ -7,10 +7,18 @@ module Notes =
         let numNote = (int note.name) + 12 * note.height
         let numNoteAfterAdd = numNote + semitones
 
-//        match numNoteAfterAdd with
-//        | n when n < 0 || n > 108 -> None
-//        | n ->
         let noteName = numNoteAfterAdd % 12
         let noteHeight = numNoteAfterAdd / 12
-        //Some
         { name = enum noteName; height = noteHeight }
+        
+    let addOpt (semitones:int) (note:Note) =
+        let numNote = (int note.name) + 12 * note.height
+        let numNoteAfterAdd = numNote + semitones
+        
+        let noteName = numNoteAfterAdd % 12
+        let noteHeight = numNoteAfterAdd / 12
+
+        match numNoteAfterAdd with
+        | n when 0 <= n && n <= 108 -> Some { name = enum noteName; height = noteHeight }
+        | _ -> None
+        
